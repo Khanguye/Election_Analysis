3b56fb12d2e802d76fbb377de53dfe4f4f1da7db
Kha Nguyen <trongkha@yahoo.com>
2020-01-25 22:04:11 -0800

    Add PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -25,6 +25,31 @@
 
 # print out the winner name with the max percentage
 
+#-------------------------
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # To do: read and analyze the data here. 
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+    for row in file_reader:
+        print(row[0])
+        break
+
+# Using the open() function with the "w" mode we will write data to the file.
+with open(file_to_save, "w") as txt_file:
+    # Write three counties to the file.
+     txt_file.writelines(["Counties in the Election\n","-"*25+"\n"])
+     txt_file.write("Arapahoe\n")
+     txt_file.write("Denver\n")
+     txt_file.write("Jefferson\n")
 
 
