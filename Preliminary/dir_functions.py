c7fef491815e94ab3ddd8a651eba10c6fa4e4e99
Kha Nguyen <trongkha@yahoo.com>
2020-01-25 16:36:09 -0800

    Pseudocoding
    Pseudocoding

---
+++ Preliminary/dir_functions.py
@@ -0,0 +1,18 @@
+import csv
+import random
+import datetime
+now = datetime.datetime.now()
+print("The time right now is,",now)
+
+print(dir(csv))
+print(dir({}))
+print(dir([]))
+print(dir(()))
+print(dir(int))
+print(dir(float))
+print(dir(bool))
+print(dir(list))
+print(dir(tuple))
+print(dir(dict))
+print(dir(datetime))
+print(dir(random))
---
+++ PyPoll.py
@@ -0,0 +1,30 @@
+# The data we need to retrieve
+# Total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate received
+# Percentage of votes each candidate won
+# The winner of the election based on popular vote
+
+# an vailable integer (total vote) to hold the counts of each line registered vote
+# need List dictionary to hold candidate names and total number of votes {"CanidateName":CanidateVotes}
+
+# open the data file and read each line data in csv (Ballot ID,County,Candidate)
+# Skip 1 read line 
+
+# Loop read line to end file
+# increase the total vote by each time read line
+# Find canidate name from dictionary
+# if found then increase the canidate votes by 1
+# else insert the new caidate name and vote = 1
+# End Loop
+
+# Loop Dictionary
+# find percentage of votes = CanidateVotes / total votes of each candidate in dictionary
+# keep the max percentage and canidate name
+# End Loop
+
+# print out the winner name with the max percentage
+
+
+
+
